3bfd8f3f172ef7154c31bc8bfe50d03b3f92975f
VeronicaTaucci <taucci.veronica@gmail.com>
2022-05-05 11:46:51 -0400

    sorting list fixed

--- src/components/SortStudentList.jsx
+++ src/components/SortStudentList.jsx
@@ -1,35 +1,54 @@
 
-import React from 'react';
+import { React, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 
 
 const SortStudentList = () => {
 
-    //sort studentlist in alphabetical order
+  //sort studentlist in alphabetical order
   const dispatch = useDispatch();
   const count = useSelector(state => state.count);
-  const students = useSelector(state => state.students)
-
-  const mapFirstName = students.map(student => {
-    return `${student.fName} , ${student.city}`
-
+  const students = useSelector(state => state.students);
+  const [sortCity, setSortCity] = useState([]);
+  const [sortName, setSortName] = useState([]);
 
 
 
-  })
-  const sortedStudents = mapFirstName.sort(function (a, b) {
-    return a.toLowerCase().localeCompare(b.toLowerCase());
-  });
-  console.log(sortedStudents);
+  const handleSortByName = () => {
+    const mapFirstName = students.map(student => {
+      return `${student.fName} , ${student.city}`
+    })
+    const sortedStudents = mapFirstName.sort(function (a, b) {
+      const sortedNames = a.toLowerCase().localeCompare(b.toLowerCase());
+    });
+    setSortCity([])
+    setSortName(sortedStudents);
+    
+  }
+
+  const handleSortByCity = () => {
+    const mapCity = students.map(student => {
+      return `${student.city} , ${student.fName}`
+    })
+    console.log(mapCity);
+    const sortedCities = mapCity.sort(function (a, b) {
+      const sortedCities1 = a.toLowerCase().localeCompare(b.toLowerCase());
+      return sortedCities1
+    });
+    setSortName([])
+    setSortCity(sortedCities)
+  }
   return (
     <>
-      <h1>Display Students</h1>
-      <h2>{count}</h2>
-      <ul>
-        {sortedStudents.map(student => {
-          return <li key={student.id}>{student}</li>
-        })}
-      </ul>
+      <h1>Sort students</h1>
+      <button onClick={handleSortByName} >sort by name</button>
+      <button onClick={handleSortByCity} >sort by city</button>
+      <ul  > {sortCity.map(city => {
+        return <li>{city}</li>
+      })}</ul>
+      <ul > {sortName.map(name => {
+        return <li>{name}</li>
+      })}</ul>
     </>
   )
 }
